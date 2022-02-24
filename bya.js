be74414d49553269822b2ed1b90899501e6a3081
Byanca <79319402+byancamari@users.noreply.github.com>
2022-02-24 19:21:38 -0300

    Add files via upload

---
+++ bya.js
@@ -0,0 +1,21 @@
+// tipo Number
+
+const meuNumero = 3;
+
+const num1= 1;
+const num2= 2;
+
+const operacaomatematica = num1 * num2
+
+console.log (operacaomatematica)
+
+// ponto flutuente 
+  const numeroPontoFlutuente = 3.3 ;
+  const numeroPontoFlutuenteSemZero = .5;
+
+  const novaOpreracao = num1/num2;
+  console.log(novaOpreracao)
+
+  // NaN -> not A Number (não é um número )
+   const alura = "Alura"
+   console.log ( alura* num1)
